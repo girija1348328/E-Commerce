0457c45e7e0139991a19f7e5af00b41210a672e5
Girija Sankar Mohanta <106576949+girija1348328@users.noreply.github.com>
2024-11-05 09:52:54 +0530

    game_section added

--- src/app/components/Layout/Header.tsx
+++ src/app/components/Layout/Header.tsx
@@ -1,91 +1,68 @@
 import React from 'react';
-import { Box, Flex, Text, Button, useColorMode, HStack, InputGroup, Input, Menu, MenuButton, MenuList, MenuItem, IconButton, Stack } from '@chakra-ui/react';
-import { ChevronDownIcon, Icon } from '@chakra-ui/icons';
+import {
+    Box, Flex, Text, Button, HStack, InputGroup, Input, Menu, MenuButton, MenuList, MenuItem, Icon, Container, Divider, Grid
+} from '@chakra-ui/react';
+import { ChevronDownIcon } from '@chakra-ui/icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeadphones, faUser, faCartShopping } from '@fortawesome/free-solid-svg-icons';
 
 const Header = () => {
-    const { colorMode, toggleColorMode } = useColorMode();
-
     return (
-        <Flex as="header" width="full" align="center" justifyContent="space-between" p={4} bg="white" color="white">
-            <Flex align="center">
-                <Text fontSize="2xl" fontWeight="bold" mr={10} color="black">
-                    Girija_Ecommerce
+        <Container maxW="100%" p={4} bg="white">
+            <Flex as="header" direction="row" align="center" justify="space-between" width="full" mb={4}>
+                <Text fontSize="2xl" fontWeight="bold" color="black">
+                    IG_Ecommerce
                 </Text>
-            </Flex>
 
-            <HStack gap="10" width="full" align="center" backgroundColor="white" width="400px">
-                <Menu>
-                    <MenuButton as={Button} rightIcon={<ChevronDownIcon />} colorScheme="white">
-                        <Text color="black">All Categories</Text>
-                    </MenuButton>
-                    <MenuList>
-                        <MenuItem>Electronics</MenuItem>
-                        <MenuItem>Clothing</MenuItem>
-                        <MenuItem>Accessories</MenuItem>
-                        <MenuItem>Home Goods</MenuItem>
-                    </MenuList>
-                </Menu>
-                <HStack width="120%">
-                    <InputGroup >
+                <HStack spacing={10}>
+                    <Menu>
+                        <MenuButton as={Button} rightIcon={<ChevronDownIcon />} colorScheme="blue">
+                            <Text color="black">Categories</Text>
+                        </MenuButton>
+                        <MenuList>
+                            <MenuItem>Electronics</MenuItem>
+                            <MenuItem>Clothing</MenuItem>
+                            <MenuItem>Accessories</MenuItem>
+                            <MenuItem>Home Goods</MenuItem>
+                        </MenuList>
+                    </Menu>
+                    <InputGroup size="md">
                         <Input placeholder="Search for products" />
+                        <Button backgroundColor="#0068c9" borderRadius="md">
+                            <Text color="white" textStyle="bold">SEARCH</Text>
+                        </Button>
                     </InputGroup>
-                    <Button background="#0068c9" borderRadius="0px">
-                        <Text color="White">SEARCH</Text>
-                    </Button>
                 </HStack>
-            </HStack>
 
-            <HStack spacing="22">
-                <Box>
-                    <Icon as={FontAwesomeIcon} icon={faHeadphones} color="black" height="40px" width="40px" />
-
-                </Box>
-                <Box>
-                    <Text color="black">
-                        Need Help?
-                    </Text>
-                    <Text color="lightblue">
-                        +08 92298228
-                    </Text>
-                </Box>
-
-                <Box>
-                    <Icon as={FontAwesomeIcon} icon={faUser} color="black" height="40px" width="40px" />
-
-                </Box>
-                <Box>
-                    <Text color="black">
-                        My Account
-                    </Text>
-                    <Text color="lightblue">
-                        Login
-                    </Text>
-                </Box>
-
-                <Box>
-                    <Icon as={FontAwesomeIcon} icon={faCartShopping} color="black" height="40px" width="40px" />
-
-                </Box>
-                <Box>
-                    <Text color="black">
-                        My Cart
-                    </Text>
-                    <Text color="lightblue">
-                        $0.00
-                    </Text>
-                </Box>
-            </HStack>
-            <Flex align="center">
-                {/* <Button onClick={toggleColorMode} mr={4}>
-                    {colorMode === 'light' ? 'Dark' : 'Light'} Mode
-                </Button> */}
-                {/* <Button colorScheme="blue" variant="outline">
-                    Log In
-                </Button> */}
+                <HStack spacing={5}>
+                    {[{ icon: faHeadphones, text: "Need Help?", subText: "+08 92298228" },
+                    { icon: faUser, text: "My Account", subText: "Login" },
+                    { icon: faCartShopping, text: "My Cart", subText: "$0.00" }
+                    ].map((item, index) => (
+                        <Flex key={index} align="center">
+                            <Icon as={FontAwesomeIcon} icon={item.icon} boxSize="10" color="black" mr={2} />
+                            <Box>
+                                <Text color="black">{item.text}</Text>
+                                <Text color="lightblue">{item.subText}</Text>
+                            </Box>
+                        </Flex>
+                    ))}
+                </HStack>
             </Flex>
-        </Flex>
+            <Divider orientation="horizontal" borderColor="gray.200" mb={4} />
+
+            <Grid templateColumns="repeat(6, 1fr)" gap={6}>
+                {['Laptop', 'Mobile', 'Headphone', 'Speaker', 'Smartphone','Games'].map((content, index) => (
+                    <Box key={index} bg="gray.100" p={4} borderRadius="md" boxShadow="md">
+                        <Text fontWeight="bold">{content}</Text>
+                    </Box>
+                ))}
+            </Grid>
+
+            <Box>
+                
+            </Box>
+        </Container>
     );
 };
 
